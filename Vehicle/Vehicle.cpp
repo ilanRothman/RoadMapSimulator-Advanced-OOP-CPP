6d5fcb4ffa5880fd6b69218812ed1b4ed1e2ee29
pazMenachem <paz.menach@gmail.com>
2022-05-13 19:03:08 +0300

    Changes by iroth

--- Vehicle/Vehicle.cpp
+++ Vehicle/Vehicle.cpp
@@ -1,29 +1,32 @@
-//
-// Created by iroth on 5/11/2022.
-//
 
 #include "Vehicle.h"
-
+using namespace std;
 
 void Vehicle::addEdge(shared_ptr<Junction>const& source,shared_ptr<Junction>const& target, int duration) {
-    auto value = graph.find(source);
+  if( !getSource(target->getName()) ){ // if target isn't in graph yet.
+    graph.insert({target,vector < pair < shared_ptr<Junction> ,int> >()});
+  }
+  // check if source exists
+    for(auto& src : graph) {
 
-    if( value != graph.end()) // Source junction found
-    {
-        for ( auto trg : value->second ) // going through all Target Junctions
-           if( trg.first->getName() == target->getName() ) // There's already an edge
-               if ( trg.second > duration )
-               {
-                   trg.second = duration;
-                   return;
-               }
-        // target hasn't been found
-        value->second.emplace_back(target,duration);
+      if (src.first->getName() == source->getName()) {
+        for (auto& trg: src.second) // going through all Target Junctions
+        {
+          if (trg.first->getName() == target->getName()) // There's already an edge
+            if (trg.second > duration) {
+              trg.second = duration;
+              return;
+            }
+        }
+        src.second.emplace_back(target, duration);
+        return;
+      }
     }
     // Source doesn't exists
     auto nTargetPair = std::make_pair(target,duration);
     vector < pair < shared_ptr<Junction> ,int> > v = {nTargetPair};
     graph.insert({source,v});
+
 }
 
 void Vehicle::printMap(){
